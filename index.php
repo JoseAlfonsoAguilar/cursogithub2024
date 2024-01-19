6b5240ca300eecb709234c3e17c9eefb220c367f
Jose Alfonso Aguilar <ing.jose.alfonso.aguilar@gmail.com>
2024-01-18 18:30:11 -0700

    Segundo commit

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
     phpinfo();
 ?>
+…or create a new repository on the command line
+echo "# cursogithub2024" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:JoseAlfonsoAguilar/cursogithub2024.git
+git push -u origin main
