eaad3dc7417474732f52e3d1cf11c31dd9d850f4
Jose Alfonso Aguilar <ing.jose.alfonso.aguilar@gmail.com>
2024-01-19 17:33:44 -0700

    Otro Cambio desde el Visual Studio Code.

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
     phpinfo();
-
-echo " Create a new repository on the command line";
-echo "# cursogithub2024 >> README.md";
-echo "git init";
-echo "git add README.md";
-echo "git commit -m first commit";
-echo "git branch -M main";
-echo "git remote add origin git@github.com:JoseAlfonsoAguilar/cursogithub2024.git";
-echo "git push -u origin main";
+    echo "<br>";
+    echo " Create a new repository on the command line";
+    echo "# cursogithub2024 >> README.md";
+    echo "git init";
+    echo "git add README.md";
+    echo "git commit -m first commit";
+    echo "git branch -M main";
+    echo "git remote add origin git@github.com:JoseAlfonsoAguilar/cursogithub2024.git";
+    echo "git push -u origin main";
 
 ?>
